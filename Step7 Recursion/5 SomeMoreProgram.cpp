5062011f29022da4f75ea023f656acd6a8fd2099
ShivamSharma1606 <161970417+ShivamSharma1606@users.noreply.github.com>
2025-08-11 13:06:08 +0000

    commit change

--- Step7 Recursion/5 SomeMoreProgram.cpp
+++ Step7 Recursion/5 SomeMoreProgram.cpp
@@ -1,53 +1,66 @@
 // Function to sort the stack using recursion
-void insert(stack<int> &st, int pos){
-    if(st.empty() || st.top() < pos){
-        st.push(pos);
-        return;
-    }
-
-    //removing all the above element which are greater than our target
-    int temp = st.top();
-    st.pop();
-    insert(st, pos);
-    st.push(temp);
-}
-
-void sortStack(stack<int> &st)
+class sortStack
 {
-    if(st.empty()){
-        return;
+    void insert(stack<int> &st, int pos)
+    {
+        if (st.empty() || st.top() < pos)
+        {
+            st.push(pos);
+            return;
+        }
+
+        // removing all the above element which are greater than our target
+        int temp = st.top();
+        st.pop();
+        insert(st, pos);
+        st.push(temp);
     }
 
-    int top = st.top();
-    st.pop();
-    //first empty the whole stack
-    sortStack(st);
-    insert(st, top);
-}
+    void sortStack(stack<int> &st)
+    {
+        if (st.empty())
+        {
+            return;
+        }
+
+        int top = st.top();
+        st.pop();
+        // first empty the whole stack
+        sortStack(st);
+        insert(st, top);
+    }
+};
 
 // Count good number and power of a number
-class Solution {
+class goodcount
+{
 public:
     const long long mod = 1e9 + 7;
 
     // answer, exp, base
     // I have find the power using recursive method we can also find using normal method check the problem 50. Pow(x, n)
-    long long mpow(long long ans, long long n, long long x) {
+    long long mpow(long long ans, long long n, long long x)
+    {
 
-        if (n == 0) {
+        if (n == 0)
+        {
             return ans;
         }
 
-        if (n % 2 == 0) {
+        if (n % 2 == 0)
+        {
             long long temp = (x * x) % mod;
             return mpow(ans, n / 2, temp);
-        } else {
+        }
+        else
+        {
             long long temp = (ans * x) % mod;
             return mpow(temp, n - 1, x);
         }
     }
 
-    int countGoodNumbers(long long n) {
+    int countGoodNumbers(long long n)
+    {
         const long long mod = 1e9 + 7;
         long long evencount = n / 2;
         long long oddcount = n - evencount;
@@ -60,13 +73,15 @@ public:
 };
 
 // Implementation of Atoi
-#include <bits/stdc++.h>
-class Solution {
+class atoi
+{
 
-    long long rans(string s, int n, long long& ans, int sign, int i) {
+    long long rans(string s, int n, long long &ans, int sign, int i)
+    {
 
         // checking the length
-        if (i == n || !isdigit(s[i])) {
+        if (i == n || !isdigit(s[i]))
+        {
             return sign * ans;
         }
 
@@ -74,14 +89,16 @@ class Solution {
         ans = (ans * 10) + digit;
 
         // cheking the bound
-        if (ans > INT_MAX) {
+        if (ans > INT_MAX)
+        {
             return (sign == 1) ? INT_MAX : INT_MIN;
         }
         return rans(s, n, ans, sign, i + 1);
     }
 
 public:
-    int myAtoi(string s) {
+    int myAtoi(string s)
+    {
 
         // recursive solution
         int n = s.size();
@@ -89,14 +106,18 @@ public:
         int i = 0;
         long long ans = 0;
         // removing whitespace
-        while (i < n && s[i] == ' ') {
+        while (i < n && s[i] == ' ')
+        {
             i++;
         }
         // cheking the sign
-        if (i < n && s[i] == '-') {
+        if (i < n && s[i] == '-')
+        {
             sign = -1;
             i++;
-        } else if (i < n && s[i] == '+') {
+        }
+        else if (i < n && s[i] == '+')
+        {
             i++;
         }
         return rans(s, n, ans, sign, i);
@@ -136,25 +157,33 @@ public:
 };
 
 // Reverse a stacking using recursion
-void insertB(stack<int> &stack, int temp){
-    if(stack.empty()){
-        stack.push(temp);
-        return;
+class reverse
+{
+    void insertB(stack<int> &stack, int temp)
+    {
+        if (stack.empty())
+        {
+            stack.push(temp);
+            return;
+        }
+
+        int top = stack.top();
+        stack.pop();
+        insertB(stack, temp);
+        stack.push(top);
     }
 
-    int top = stack.top();
-    stack.pop();
-    insertB(stack, temp);
-    stack.push(top);
-}
+    void reverseStack(stack<int> &stack)
+    {
+        if (stack.empty())
+        {
+            return;
+        }
 
-void reverseStack(stack<int> &stack) {
-    if(stack.empty()){
-        return;
+        int temp = stack.top();
+        stack.pop();
+        reverseStack(stack);
+        insertB(stack, temp);
     }
+};
 
-    int temp = stack.top();
-    stack.pop();
-    reverseStack(stack);
-    insertB(stack, temp);
-}
